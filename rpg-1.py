8b3d628c1e86dde1abd60002cebce932f850ecae
Patrick Furtak <patrick@patrickfurtak.com>
2020-01-22 11:59:58 -0500

    Added critical hit feature with Victor

--- rpg-1.py
+++ rpg-1.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Hero:
     health = 10
     power = 5
@@ -8,14 +11,17 @@ class Hero:
 
     def attack(self, greens):
         # Hero attacks goblin
-        greens.health -= russle.power
-        print("You do %d damage to the goblin." % russle.power)
+        critical = random.random() > .8
+        russle.crit = russle.power * 2
+        if critical:
+            print('Critical!')
+            greens.health -= russle.crit
+            print("You do %d damage to the goblin." % russle.crit)
+        else:
+            greens.health -= russle.power
+            print("You do %d damage to the goblin." % russle.power)
         if greens.health <= 0:
             print("The goblin is dead.")
-        elif user_input == "2":
-            pass
-        elif user_input == "3":
-            print("Goodbye.")
 
     def alive(self):
         if self.health > 0:
@@ -49,23 +55,31 @@ class Goblin:
 russle = Hero(10, 5)
 greens = Goblin(6, 2)
 
-while greens.alive() and russle.alive():
-    print("You have %d health and %d power." % (russle.health, russle.power))
-    print("The goblin has %d health and %d power." %
-          (greens.health, greens.power))
-    print()
-    print("What do you want to do?")
-    print("1. fight goblin")
-    print("2. do nothing")
-    print("3. flee")
-    print("> ",)
-    user_input = input()
-    if user_input == "1":
-        russle.attack(greens)
-    elif user_input == "2":
-        pass
-    elif user_input == "3":
-        print("Goodbye.")
-        break
-    else:
-        print("Invalid input %r" % user_input)
+
+def main():
+
+    while greens.alive() and russle.alive():
+        print("You have %d health and %d power." %
+              (russle.health, russle.power))
+        print("The goblin has %d health and %d power." %
+              (greens.health, greens.power))
+        print()
+        print("What do you want to do?")
+        print("1. fight goblin")
+        print("2. do nothing")
+        print("3. flee")
+        print("> ",)
+        user_input = input()
+        if user_input == "1":
+            russle.attack(greens)
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            print('Good Bye.')
+            break
+
+        else:
+            print("Invalid input %r" % user_input)
+
+
+main()
